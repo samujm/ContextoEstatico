4991de599dfd09f724ce716208f1e7685c33fdb6
Samantha González <samujm76@gmail.com>
2022-08-12 12:29:45 -0500

    Si el contadorPersona no fuera static este siempre se reiniciaria ya que estaría asociado a la clase y no al objeto

--- src/test/PersonaPrueba.java
+++ src/test/PersonaPrueba.java
@@ -1,7 +1,14 @@
 
 package test;
 
+import domain.Persona;
 
 public class PersonaPrueba {
-    
+    public static void main(String[] args) {
+        Persona persona1 = new Persona("Juan");
+        System.out.println("persona1 = " + persona1);
+        
+        Persona persona2 = new Persona("Karla"); //Si el contadorPersona no fuera static este siempre se reiniciaria ya que estaría asociado a la clase y no al objeto
+        System.out.println("persona2 = " + persona2);
+    }
 }
